1c24b79d7b54cdc745c0e29d1a58335a1c1c503e
Kat <katkatvdd@yahoo.com.ph>
2022-04-08 14:14:15 +0800

    Modified Book constructor to Class

--- static/script.js
+++ static/script.js
@@ -1,12 +1,12 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function(){
-        return info;
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
 }
 
